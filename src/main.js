59b8c3fba582d95f15dd38e036e3c46a5c9f9796
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2025-12-04 22:08:02 -0500

    Revert "max delta debug"
    This reverts commit 78a3bee809c58dfa303a89ea5ca0ce05408f653d.

--- src/main.js
+++ src/main.js
@@ -49,9 +49,6 @@ function formatObject(obj, indent = 0) {
 let SPINNER_P1_DELTA_TOTAL = 0;
 let SPINNER_P2_DELTA_TOTAL = 0;
 
-let SPINNER_P1_DELTA_MAX = 0;
-let SPINNER_P2_DELTA_MAX = 0;
-
 // Update display every frame
 function updateDisplay() {
   // Reset knobs when both start buttons are pressed simultaneously
@@ -66,11 +63,9 @@ function updateDisplay() {
   const spinner_P1 = SPINNER_P1.SPINNER;
   const delta_P1 = spinner_P1.step_delta;
   SPINNER_P1_DELTA_TOTAL += delta_P1;
-  SPINNER_P1_DELTA_MAX = Math.max(delta_P1, SPINNER_P1_DELTA_MAX);
 
   const DEBUG_SPINNER_P1 = {
     delta: delta_P1,
-    max_delta: SPINNER_P1_DELTA_MAX,
     total_d: SPINNER_P1_DELTA_TOTAL,
     angle_16: SPINNER_P1_DELTA_TOTAL / 16,
     angle_64: SPINNER_P1_DELTA_TOTAL / 64,
@@ -82,11 +77,9 @@ function updateDisplay() {
   const spinner_P2 = SPINNER_P2.SPINNER;
   const delta_P2 = spinner_P2.step_delta;
   SPINNER_P2_DELTA_TOTAL += delta_P2;
-  SPINNER_P2_DELTA_MAX = Math.max(delta_P1, SPINNER_P1_DELTA_MAX);
 
   const DEBUG_SPINNER_P2 = {
     delta: delta_P2,
-    max_delta: SPINNER_P2_DELTA_MAX,
     total_d: SPINNER_P2_DELTA_TOTAL,
     angle_16: SPINNER_P2_DELTA_TOTAL / 16,
     angle_64: SPINNER_P2_DELTA_TOTAL / 64,
