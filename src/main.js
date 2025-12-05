33da0e81720b4e4603ed0bfc59ee441a42a46a1f
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2025-12-04 22:08:12 -0500

    Revert "bump"
    This reverts commit 1755c22e58a37839ced3ce9245a248bac212447e.

--- src/main.js
+++ src/main.js
@@ -57,12 +57,11 @@ function updateDisplay() {
     SPINNER_P2.SPINNER.reset();
   }
 
-  const spinner_P1 = SPINNER_P1.SPINNER;
-  const delta_P1 = spinner_P1.step_delta;
+  const delta_P1 = SPINNER_P1.step_delta;
   SPINNER_P1_DELTA_TOTAL += delta_P1;
 
   const DEBUG_SPINNER_P1 = {
-    angle: spinner_P1.angle,
+    angle: SPINNER_P1.angle,
     delta: delta_P1,
     total_d: SPINNER_P1_DELTA_TOTAL,
     angle_16: SPINNER_P1_DELTA_TOTAL / 16,
@@ -71,12 +70,11 @@ function updateDisplay() {
     angle_1024: SPINNER_P1_DELTA_TOTAL / 1024,
   }
 
-  const spinner_P2 = SPINNER_P2.SPINNER;
-  const delta_P2 = spinner_P2.step_delta;
+  const delta_P2 = SPINNER_P1.step_delta;
   SPINNER_P2_DELTA_TOTAL += delta_P2;
 
   const DEBUG_SPINNER_P2 = {
-    angle: spinner_P2.angle,
+    angle: SPINNER_P2.angle,
     delta: delta_P2,
     total_d: SPINNER_P2_DELTA_TOTAL,
     angle_16: SPINNER_P2_DELTA_TOTAL / 16,
